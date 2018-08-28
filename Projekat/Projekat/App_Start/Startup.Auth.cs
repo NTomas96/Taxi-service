b07e22ae4ccee883256ed1ff1499fbd5b7bdff33
NTomas96 <tomasevic.nemanja.va@gmail.com>
2018-08-28 07:58:11 +0200

    Novi pocetak

---
+++ Projekat/Projekat/App_Start/Startup.Auth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.DataProtection;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using Projekat.Models;
+using Projekat.Providers;
+
+namespace Projekat
+{
+    public partial class Startup
+    {
+        // Enable the application to use OAuthAuthorization. You can then secure your Web APIs
+        static Startup()
+        {
+
+        }
+    }
+}
