4e99ff6cd5c92ca0cd49cbf6ff4999bd4f195784
NTomas96 <tomasevic.nemanja.va@gmail.com>
2018-08-29 08:34:58 +0200

    Database setup

---
+++ WebProjekat/WebProjekat/Database/SQLiteHelper/SQLiteTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProjekat.Database.SQLiteHelper
+{
+    public class SQLiteTable
+    {
+        public string TableName = "";
+        public SQLiteColumnList Columns = new SQLiteColumnList();
+
+        public SQLiteTable()
+        { }
+
+        public SQLiteTable(string name)
+        {
+            TableName = name;
+        }
+    }
+}
